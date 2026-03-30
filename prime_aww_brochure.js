21d416f8a982c968454d0945118c6290483adc09
behradhajimohammadi-jpg <behradhajimohammadi@gmail.com>
2026-03-29 21:38:53 -0700

    Initialize prime-brochure project with package.json

---
+++ prime_aww_brochure.js
@@ -0,0 +1,12 @@
+{
+  "name": "prime-brochure",
+  "version": "1.0.0",
+  "description": "Prime Maintenance x AWM Alliance brochure generator",
+  "main": "prime_aww_brochure.js",
+  "scripts": {
+    "build": "node prime_aww_brochure.js"
+  },
+  "dependencies": {
+    "pptxgenjs": "^3.12.0"
+  }
+}
